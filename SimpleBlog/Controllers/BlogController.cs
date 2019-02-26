19a628cab49e4bd32811b7cc0b864945a630c9c8
chMatvey <morganrush@yandex.ru>
2019-02-26 23:41:27 +0300

    migration

---
+++ SimpleBlog/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using DBRepository.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SimpleBlog.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlogController : Controller
+    {
+        readonly IBlogRepository blogRepository;
+
+        public BlogController(IBlogRepository blogRepository)
+        {
+            this.blogRepository = blogRepository;
+        }
+    }
+}
